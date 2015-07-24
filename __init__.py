76b2324d9c10d566e7aa7c3dbace10949558f2dc
Alexandre Neto <senhor.neto@gmail.com>
2015-07-24 15:28:06 +0100

    Initial commit with plugin structure

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ gison3dmap
+                                 A QGIS plugin
+ Client for gison3dmap to project gis data in a physical terrain model
+                             -------------------
+        begin                : 2015-07-24
+        copyright            : (C) 2015 by Alexandre Neto (for CCC Geomática)
+        email                : alex.f.neto@gmail.com
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load gison3dmap class from file gison3dmap.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .gison3dmap import gison3dmap
+    return gison3dmap(iface)
